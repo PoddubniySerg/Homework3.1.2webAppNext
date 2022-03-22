e99c6489c6b9863d85195bd2aeb8a6dfc824e6b5
Poddubniy Sergey <poddubniysergey1981@gmail.com>
2022-03-22 19:21:31 +0300

    add try-with-resources

--- src/main/java/MyClient.java
+++ src/main/java/MyClient.java
@@ -10,23 +10,26 @@ public class MyClient {
     private final static int PORT = 8080;
     private final static String HOST = "netology.homework";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = "";
+        String input;
         //клиент
-        Socket clientSocket = new Socket(HOST, PORT);
-        PrintWriter outClient = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader inClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        System.out.println(inClient.readLine());
-        outClient.println(scanner.nextLine());
-
-        while (true) {
+        try (Socket clientSocket = new Socket(HOST, PORT);
+             PrintWriter outClient = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader inClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
             System.out.println(inClient.readLine());
-            input = scanner.nextLine();
-            outClient.println(input);
-            System.out.println(inClient.readLine() + "\n\n");
-            if (input.equals("end")) return;
+            outClient.println(scanner.nextLine());
+
+            while (true) {
+                System.out.println(inClient.readLine());
+                System.out.println(inClient.readLine());
+                input = scanner.nextLine();
+                outClient.println(input);
+                System.out.println(inClient.readLine() + "\n\n");
+                if (input.equals("end") || input.equals("close")) return;
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 }
--- src/main/java/MyLittleServer.java
+++ src/main/java/MyLittleServer.java
@@ -9,37 +9,49 @@ public class MyLittleServer {
 
     private final static int PORT = 8080;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //сервер
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        Socket clientServerSocket = serverSocket.accept();
-        PrintWriter outSrv = new PrintWriter(clientServerSocket.getOutputStream(), true);
-        BufferedReader inSrv = new BufferedReader(new InputStreamReader(clientServerSocket.getInputStream()));
-        outSrv.println("Hello! Write your name.");
-        String name = inSrv.readLine();
-
-        while (true) {
-            outSrv.println("Are you child? (yes/no) or enter \"end\" for exit.");
-            switch (inSrv.readLine()) {
-                case "yes":
-                    outSrv.println(String.format("Welcome to the kids area, %s! Let's play!", name));
-                    break;
-                case "no":
-                    outSrv.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
-                    break;
-                case "end":
-                    outSrv.println("Bye!");
-                    clientServerSocket = serverSocket.accept();
-                    outSrv = new PrintWriter(clientServerSocket.getOutputStream(), true);
-                    inSrv = new BufferedReader(new InputStreamReader(clientServerSocket.getInputStream()));
-                    outSrv.println("Hello! Write your name.");
-                    name = inSrv.readLine();
-                    break;
-                default:
-                    outSrv.println("I don't understand. Try again. (yes/no)");
-                    break;
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (!serverSocket.isClosed()) {
+                if (isServerClosed(serverSocket)) serverSocket.close();
             }
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
 
+
+    }
+
+    public static boolean isServerClosed(ServerSocket serverSocket) {
+        try (Socket clientServerSocket = serverSocket.accept();
+             PrintWriter outSrv = new PrintWriter(clientServerSocket.getOutputStream(), true);
+             BufferedReader inSrv = new BufferedReader(new InputStreamReader(clientServerSocket.getInputStream()))) {
+            outSrv.println("Hello! Write your name.");
+            String name = inSrv.readLine();
+
+            while (!clientServerSocket.isClosed()) {
+                outSrv.println("Are you child? (yes/no)\nEnter \"end\" for exit and \"close\" for stop server.");
+                switch (inSrv.readLine()) {
+                    case "yes":
+                        outSrv.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+                        break;
+                    case "no":
+                        outSrv.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+                        break;
+                    case "end":
+                        outSrv.println("Bye!");
+                        return false;
+                    case "close":
+                        outSrv.println("Bye!");
+                        return true;
+                    default:
+                        outSrv.println("I don't understand. Try again. (yes/no)");
+                        break;
+                }
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return false;
     }
 }
